041e1077de44a93cb551c52583ebfdb3264e59b9
asgarov1 <asgarov1@gmail.com>
2020-03-28 22:07:13 +0100

    did most of CourseCRUD page

--- src/main/java/com/asgarov/university/schedule/controller/CourseController.java
+++ src/main/java/com/asgarov/university/schedule/controller/CourseController.java
@@ -1,22 +1,55 @@
 package com.asgarov.university.schedule.controller;
 
+import com.asgarov.university.schedule.dao.exception.DaoException;
 import com.asgarov.university.schedule.domain.Course;
+import com.asgarov.university.schedule.domain.Professor;
 import com.asgarov.university.schedule.service.CourseService;
+import com.asgarov.university.schedule.service.ProfessorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 @Controller
+@RequestMapping("course")
 public class CourseController {
     private CourseService courseService;
+    private ProfessorService professorService;
 
-    public CourseController(CourseService courseService) {
+    public CourseController(CourseService courseService, ProfessorService professorService) {
         this.courseService = courseService;
+        this.professorService = professorService;
     }
 
-    @GetMapping("course/{id}/lectures")
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("courses", courseService.findAll());
+        return "course";
+    }
+
+    @PostMapping("/searchCoursesById")
+    public String searchUsersById(@RequestParam Long id, Model model) {
+        model.addAttribute("courses", Collections.singletonList(courseService.findById(id)));
+        return "course";
+    }
+
+    @GetMapping("/searchAll")
+    public String searchAll(Model model) {
+        model.addAttribute("courses", courseService.findAll());
+        return "course";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteCourse(@PathVariable Long id, Model model) throws DaoException {
+        courseService.deleteById(id);
+        model.addAttribute("courses", courseService.findAll());
+        return "course";
+    }
+
+
+    @GetMapping("/{id}/lectures")
     public String showCourseLectures(@PathVariable Long id, Model model) {
         Course course = courseService.findById(id);
         model.addAttribute("course", course);
@@ -24,8 +57,27 @@ public class CourseController {
         return "courseLectures";
     }
 
+    @GetMapping("/{id}/students")
+    public String showCourseStudents(@PathVariable Long id, Model model) {
+        Course course = courseService.findById(id);
+        model.addAttribute("course", course);
+        model.addAttribute("students", course.getRegisteredStudents());
+        return "courseStudents";
+    }
+
     @ModelAttribute("courseService")
     public CourseService courseService() {
         return courseService;
     }
+
+    @ModelAttribute("newCourse")
+    public Course newCourse() {
+        return new Course();
+    }
+
+    @ModelAttribute("professors")
+    public List<Professor> professors() {
+        return professorService.findAll();
+    }
+
 }
---
+++ src/main/java/com/asgarov/university/schedule/controller/RoomController.java
@@ -0,0 +1,9 @@
+package com.asgarov.university.schedule.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("room")
+public class RoomController {
+}
